e6fc1f8da49a143cedf97f1dcba3406b7c0d228e
bly852 <340926187@gapps.yrdsb.ca>
2020-04-01 07:27:23 -0400

    Added the ability for players to move

--- pong.py
+++ pong.py
@@ -4,6 +4,7 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Pong"
 player_movespeed = 300
+player_height = 50
 
 
 class MyGame(arcade.Window):
@@ -16,8 +17,15 @@ class MyGame(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLACK)
 
-        # If you have sprite lists, you should create them here,
-        # and set them to None
+        # initialize player 1 variables
+        self.player1_y = None
+        self.player1_up = None
+        self.player1_down = None
+
+        # initialize player 2 variables
+        self.player2_y = None
+        self.player2_up = None
+        self.player2_down = None
 
     def setup(self):
         """
@@ -32,7 +40,7 @@ class MyGame(arcade.Window):
         # player 2 variables
         self.player2_y = SCREEN_HEIGHT / 2
         self.player2_up = False
-        self.player_down= = False
+        self.player2_down = False
 
     def on_draw(self):
         """
@@ -44,10 +52,10 @@ class MyGame(arcade.Window):
         arcade.start_render()
 
         # player 1
-        arcade.draw_rectangle_filled((SCREEN_WIDTH / 2 - SCREEN_WIDTH / 3), self.player1_y, 10, 50, arcade.color.WHITE)
+        arcade.draw_rectangle_filled((SCREEN_WIDTH / 2 - SCREEN_WIDTH / 3), self.player1_y, 10, player_height, arcade.color.WHITE)
 
         # player 2
-        arcade.draw_rectangle_filled((SCREEN_WIDTH / 2 + SCREEN_WIDTH / 3), self.player2_y, 10, 50, arcade.color.WHITE)
+        arcade.draw_rectangle_filled((SCREEN_WIDTH / 2 + SCREEN_WIDTH / 3), self.player2_y, 10, player_height, arcade.color.WHITE)
 
     def on_update(self, delta_time):
         """
@@ -55,7 +63,17 @@ class MyGame(arcade.Window):
         Normally, you'll call update() on the sprite lists that
         need it.
         """
-        pass
+        # player 1 movement
+        if self.player1_up:
+            self.player1_y += player_movespeed * delta_time
+        if self.player1_down:
+            self.player1_y -= player_movespeed * delta_time
+
+        # player 2 movement
+        if self.player2_up:
+            self.player2_y += player_movespeed * delta_time
+        if self.player2_down:
+            self.player2_y -= player_movespeed * delta_time
 
     def on_key_press(self, key, key_modifiers):
         """
@@ -64,13 +82,33 @@ class MyGame(arcade.Window):
         For a full list of keys, see:
         http://arcade.academy/arcade.key.html
         """
-        pass
+        # player 1 movement
+        if key == arcade.key.W:
+            self.player1_up = True
+        elif key == arcade.key.S:
+            self.player1_down = True
+
+        # player 2 movement
+        if key == arcade.key.UP:
+            self.player2_up = True
+        elif key == arcade.key.DOWN:
+            self.player2_down = True
 
     def on_key_release(self, key, key_modifiers):
         """
         Called whenever the user lets off a previously pressed key.
         """
-        pass
+        # player 1 movement
+        if key == arcade.key.W:
+            self.player1_up = False
+        elif key == arcade.key.S:
+            self.player1_down = False
+
+        # player 2 movement
+        if key == arcade.key.UP:
+            self.player2_up = False
+        elif key == arcade.key.DOWN:
+            self.player2_down = False
 
     def on_mouse_motion(self, x, y, delta_x, delta_y):
         """
